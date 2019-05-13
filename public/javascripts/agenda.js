6406a97762def6a67e5bd34dcab049176005c50c
Raul Bogdan <raulmorarbogdan@gmail.com>
2019-05-13 21:38:40 +0300

    working edit

--- public/javascripts/agenda.js
+++ public/javascripts/agenda.js
@@ -1,32 +1,39 @@
 var allPersons = [];
+var editPersonId;
 
 var API_URL = {
+    CREATE: '...',
+    READ: '...',
     //ADD: 'data/add.json'
-    ADD: 'users/add',
+    ADD: 'users/add', // TODO use CREATE
+    UPDATE: 'users/update',
     DELETE: 'users/delete'
 };
 var API_METHOD = {
+    CREATE: 'POST',
+    READ: 'GET',
     //ADD: 'GET'
-    ADD: 'POST',
+    ADD: 'POST', // TODO use CREATE
+    UPDATE: 'PUT',
     DELETE: 'DELETE'
 }
 
-fetch('data/persons.json').then(function (r) {
+fetch('data/persons.json').then(function(r){
     return r.json();
-}).then(function (persons) {
+}).then(function(persons) {
     console.log('all persons', persons);
     allPersons = persons;
     display(persons);
 });
 
 function display(persons) {
-    var list = persons.map(function (person) {
+    var list = persons.map(function(person) {
         return `<tr data-id="${person.id}">
-                       <td>${person.firstName}</td>
-                       <td>${person.lastName}</td>
-                       <td>${person.phone}</td>
+            <td>${person.firstName}</td>
+            <td>${person.lastName}</td>
+            <td>${person.phone}</td>
             <td>
-                <a href="#" class="delete">&#10008;</a>
+                <a href="#" class="delete">&#10006;</a>
                 <a href="#" class="edit">&#9998;</a>
             </td>
         </tr>`;
@@ -39,30 +46,63 @@ function savePerson() {
     var firstName = document.querySelector('[name=firstName]').value;
     var lastName = document.querySelector('[name=lastName]').value;
     var phone = document.querySelector('[name=phone]').value;
-
-    submitNewPerson(firstName, lastName, phone);
+    
+    if (editPersonId) {
+        submitEditPerson(editPersonId, firstName, lastName, phone);
+    } else {
+        submitNewPerson(firstName, lastName, phone);
+    }
 }
 
 function submitNewPerson(firstName, lastName, phone) {
     var body = null;
-    if (API_METHOD.ADD === 'POST') {
+    const method = API_METHOD.ADD;
+    if (method === 'POST') {
         body = JSON.stringify({
-            firstName: firstName,
-            lastName: lastName,
-            phone: phone
+            firstName,
+            lastName,
+            phone
         });
     }
     fetch(API_URL.ADD, {
-        method: API_METHOD.ADD,
-        body: body,
+        method,
+        body,
         headers: {
             "Content-Type": "application/json"
         }
-    }).then(function (r) {
+    }).then(function(r) {
         return r.json();
-    }).then(function (status) {
+    }).then(function(status) {
         if (status.success) {
             inlineAddPerson(status.id, firstName, lastName, phone);
+        } else {
+            console.warn('not saved!', status);
+        }
+    })
+}
+
+function submitEditPerson(id, firstName, lastName, phone) {
+    var body = null;
+    const method = API_METHOD.UPDATE;
+    if (method === 'PUT') {
+        body = JSON.stringify({
+            id,
+            firstName,
+            lastName,
+            phone
+        });
+    }
+    fetch(API_URL.UPDATE, {
+        method,
+        body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function(r) {
+        return r.json();
+    }).then(function(status) {
+        if (status.success) {
+            inlineEditPerson(id, firstName, lastName, phone);
         } else {
             console.warn('not saved!', status);
         }
@@ -73,16 +113,34 @@ function submitNewPerson(firstName, lastName, phone) {
 function inlineAddPerson(id, firstName, lastName, phone) {
     allPersons.push({
         id,
-        firstName: firstName,
-        lastName: lastName,
-        phone: phone
+        firstName,
+        lastName,
+        phone
     });
     display(allPersons);
 }
 
+function inlineEditPerson(id, firstName, lastName, phone) {
+    //window.location.reload();
+    
+    const person = allPersons.find((p) => {
+        return p.id == id;
+    });
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.phone = phone;
+    
+    display(allPersons);
+
+    editPersonId = '';
+    document.querySelector('[name=firstName]').value = '';
+    document.querySelector('[name=lastName]').value = '';
+    document.querySelector('[name=phone]').value = '';
+}
+
 function inlineDeletePerson(id) {
-    console.warn('please refresh :)' ,id);
-    allPersons =allPersons.filter(function(person){
+    console.warn('please refresh :)', id);
+    allPersons = allPersons.filter(function(person) {
         return person.id != id;
     });
     display(allPersons);
@@ -91,7 +149,7 @@ function inlineDeletePerson(id) {
 function deletePerson(id) {
     var body = null;
     if (API_METHOD.DELETE === 'DELETE') {
-        body = JSON.stringify({id});
+        body = JSON.stringify({ id });
     }
     fetch(API_URL.DELETE, {
         method: API_METHOD.DELETE,
@@ -99,9 +157,9 @@ function deletePerson(id) {
         headers: {
             "Content-Type": "application/json"
         }
-    }).then(function (r) {
+    }).then(function(r) {
         return r.json();
-    }).then(function (status) {
+    }).then(function(status) {
         if (status.success) {
             inlineDeletePerson(id);
         } else {
@@ -110,16 +168,29 @@ function deletePerson(id) {
     })
 }
 
+const editPerson = function(id) {
+    var person = allPersons.find(function(p) {
+        return p.id == id
+    });
+    document.querySelector('[name=firstName]').value = person.firstName;
+    document.querySelector('[name=lastName]').value = person.lastName;
+    document.querySelector('[name=phone]').value = person.phone;
+    editPersonId = id;
+}
+
 function initEvents() {
     const tbody = document.querySelector('#agenda tbody');
-    tbody.addEventListener('click', function (e) {
-        console.warn('click on tbody', e.target);
+    tbody.addEventListener('click', function(e) {
         if (e.target.className == 'delete') {
             const tr = e.target.parentNode.parentNode;
             const id = tr.getAttribute('data-id');
             deletePerson(id);
+        } else if (e.target.className == 'edit') {
+            const tr = e.target.parentNode.parentNode;
+            const id = tr.getAttribute('data-id');
+            editPerson(id);
         }
     });
-}   
+}
 
-initEvents()
+initEvents();
--- routes/users.js
+++ routes/users.js
@@ -3,54 +3,86 @@ var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 // http://localhost:3000/users/add
-router.post('/add', function (req, res, next) {
+router.post('/add', function(req, res, next) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var phone = req.body.phone;
   console.warn('add', firstName, lastName, phone);
-
+  
   var persons = require('../public/data/persons.json');
-
-  const id = new Date().getTime()
+  //var strPersons = fs.readFileSync('./public/data/persons.json');
+  //var persons = JSON.parse(strPersons);
+  
+  const id = new Date().getTime();
   persons.push({
     id,
     firstName,
     lastName,
     phone
   });
-
+  
   var str = JSON.stringify(persons, null, 2);
   fs.writeFileSync('./public/data/persons.json', str);
-
 
   res.json({
     success: true,
     id,
-    message: 'DONE'
+    message: 'Done!'
   });
 });
 
-router.delete('/delete', function (req, res, next) {
-  var id = req.body.id;
-  console.warn('remove person', id);
 
+router.put('/update', function(req, res, next) {
+  const id = req.body.id;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const phone = req.body.phone;
+  console.warn('update', id, firstName, lastName, phone);
+  
   var persons = require('../public/data/persons.json');
-
-  var remainingPersons = persons.filter(function (person) {
-    return person.id != id;
-  })
-  var str = JSON.stringify(remainingPersons, null, 2);
+  
+  // update...
+  // const person = persons.find(function(p){
+  //   return p.id == id;
+  // });
+  const person = persons.find((p) => {
+    return p.id == id;
+  });
+  person.firstName = firstName;
+  person.lastName = lastName;
+  person.phone = phone;
+  
+  var str = JSON.stringify(persons, null, 2);
   fs.writeFileSync('./public/data/persons.json', str);
-
 
   res.json({
     success: true,
-    message: 'DONE'
+    id,
+    message: 'Done!'
+  });
+});
+
+router.delete('/delete', function(req, res, next) {
+  var id = req.body.id;
+  console.warn('remove person', id);
+  
+  var persons = require('../public/data/persons.json');
+  
+  var remainingPersons = persons.filter(function(person) {
+    return person.id != id; 
+  });
+  
+  var str = JSON.stringify(remainingPersons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);
+
+  res.json({
+    success: true,
+    message: 'Done!'
   });
 });
 
